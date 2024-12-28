a14a498d560b65552f39acec99c9c12d17831d8f
Dev Mik <mikdev4sec@gmail.com>
2024-12-28 09:20:03 +0300

    pep8

--- blogicum/blog/forms.py
+++ blogicum/blog/forms.py
@@ -22,4 +22,3 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email')
-        
--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -97,4 +97,3 @@ class Comment(models.Model):
 
     def __str__(self) -> str:
         return self.text
-    
